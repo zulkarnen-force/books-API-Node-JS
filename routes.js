93df5066ce7b28f1ea33ec3a4dd03f06143d02ab
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-21 16:17:14 +0700

    create file routes to handle noun endpoint /books

---
+++ routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+const routeBooks = require('./routes/books.route');
+const routes = express.Router();
+
+
+routes.use('/books', routeBooks)
+
+
+
+module.exports = routes;
