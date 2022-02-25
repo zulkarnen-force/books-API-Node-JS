78483ad6007e2cd32aae3b8264fa139ae80af9ea
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-25 08:22:36 +0700

    create schema for table Publisher

---
+++ migrations/1645752059228_create-table-Publisher.js
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.createTable('publisher', {
+        publisher_id: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+            primaryKey: true,
+         }, 
+         name:{
+             type: 'VARCHAR(50)',
+             notNull: true
+         },
+         city:{
+            type: 'VARCHAR(50)'
+        },
+         created_at: {
+             type: 'TEXT',
+             notNull: true
+         },
+         updated_at:{
+             type: 'TEXT', 
+             notNull: true
+         },
+    })
+};
+
+exports.down = pgm => {
+    pgm.dropTable('publisher')
+};
