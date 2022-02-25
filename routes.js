462d947a6b372a9507805a0c084b87acade4038b
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-25 09:43:58 +0700

    add new route for /authors

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routesAuthor = require('./routes/author.route');
 const app = express()
 const routeBooks = require('./routes/books.route');
 const routesPublisher = require('./routes/publisher.route');
@@ -8,6 +9,8 @@ const routes = express.Router();
 
 routes.use('/books', routeBooks);
 routes.use('/publishers', routesPublisher);
+routes.use('/authors', routesAuthor);
+
 
 
 
