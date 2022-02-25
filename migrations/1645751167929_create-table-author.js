8daf028f0162c0d4aad2ab798d03f862821b618a
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-25 08:09:39 +0700

    create schema for table Author

--- migrations/1645751167929_create-table-author.js
+++ migrations/1645751167929_create-table-author.js
@@ -2,6 +2,28 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = pgm => {
+    pgm.createTable('author', {
+        author_id: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+            primaryKey: true,
+         }, 
+         name:{
+             type: 'VARCHAR(50)',
+             notNull: true
+         },
+         created_at: {
+             type: 'TEXT',
+             notNull: true
+         },
+         updated_at:{
+             type: 'TEXT', 
+             notNull: true
+         },
+    })
+};
 
-exports.down = pgm => {};
+exports.down = pgm => {
+    pgm.dropTable('author')
+};
